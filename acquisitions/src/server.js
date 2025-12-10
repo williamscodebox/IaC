e064337b5765718d3c824d2799a9027bc4a49fdb
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-10 16:40:09 -0500

    update server

--- acquisitions/src/server.js
+++ acquisitions/src/server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });

7ce8490fb7c06598b6abf0f08f602daf1ce8cbfb
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-10 17:39:41 -0500

    update drizzle config

--- acquisitions/drizzle.config.js
+++ acquisitions/drizzle.config.js
@@ -1,5 +1,9 @@
 import 'dotenv/config';
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in the .env file');
+}
+
 export default {
   schema: './src/models/*.js',
   out: './drizzle',

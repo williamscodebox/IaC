2964c23a82c0d42075934acf410a36c31c0b386b
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-11 13:44:33 -0500

    update logger

--- acquisitions/src/config/logger.js
+++ acquisitions/src/config/logger.js
@@ -2,7 +2,7 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: winston.combine(
+  format: winston.format.combine(
     (winston.format.timestamp(),
     winston.format.errors({ stack: true }),
     winston.format.json())

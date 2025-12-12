510b2e9ff5269c1feb708a73ad3c88c511b7141b
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-12 15:29:21 -0500

    update app

--- acquisitions/src/app.js
+++ acquisitions/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import logger from '#config/logger.js';
-// import helmet from "helmet";
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');

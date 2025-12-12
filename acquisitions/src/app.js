2243bbf040519f1efd4c8b8d06ca98bae58ce18e
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-12 15:40:06 -0500

    update packages

--- acquisitions/src/app.js
+++ acquisitions/src/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import logger from '#config/logger.js';
 import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');

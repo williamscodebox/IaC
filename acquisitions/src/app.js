cf7af2a5a0b0ad9430bf0a925c38badd87344e44
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-12 15:42:21 -0500

    update app file

--- acquisitions/src/app.js
+++ acquisitions/src/app.js
@@ -3,6 +3,7 @@ import logger from '#config/logger.js';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(
   morgan('combined', {

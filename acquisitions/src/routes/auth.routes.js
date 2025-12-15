b10b70f632afb9aa440b7f820c5e1c29ddedbba0
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-15 17:10:24 -0500

    update auth routes

--- acquisitions/src/routes/auth.routes.js
+++ acquisitions/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signIn, signup } from '../controllers/auth.controller.js';
+import { signIn, signOut, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -15,8 +15,10 @@ router.post('/sign-up', signup);
 
 router.post('/sign-in', signIn);
 
-router.post('/sign-out', (req, res) => {
-  res.send('POST /api/auth/sign-out response');
-});
+// router.post('/sign-out', (req, res) => {
+//   res.send('POST /api/auth/sign-out response');
+// });
+
+router.post('/sign-out', signOut);
 
 export default router;

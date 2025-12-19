647b67217a3a31dc7af30807be3ff412d640be27
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-19 00:04:30 -0500

    update app user routes

--- acquisitions/src/app.js
+++ acquisitions/src/app.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
 import securityMiddleware from './middleware/security.middleware.js';
+import usersRoutes from './routes/users.routes.js';
 
 const app = express();
 
@@ -41,7 +42,7 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
-// app.use('/api/users', usersRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
